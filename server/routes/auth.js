82a980071fb7f377a52c313a9279e92068059293
Sharath Akkaldevi <90202583+sharath-123@users.noreply.github.com>
2025-05-28 18:25:28 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const User = require('../models/User');
 const multer = require('multer');
 const path = require('path');
 const storage = multer.diskStorage({
